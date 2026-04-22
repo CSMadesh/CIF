a222293090dff21d39b0004a18492ee447eb57d7
CSMadesh <madeshcs28@gmail.com>
2026-04-22 13:24:00 +0530

    fix vercel deployment

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,9 @@
 import os
 import sys
 
-# Add project root to path
-sys.path.insert(0, os.path.dirname(__file__))
-
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ixova.settings')
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
-# Vercel needs this named 'app'
-app = application
+app = get_wsgi_application()
